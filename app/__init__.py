31969b8a98ce891e312f3d5c54e3c8905f6002ba
happiness <happinesskawinzi@gmail.com>
2019-09-09 08:11:41 +0300

    create a bootstrap navbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from .config import DevConfig
+import bootstrap
 
 #initializing application
 app = Flask(__name__,instance_relative_config= True)
@@ -8,5 +9,8 @@ app = Flask(__name__,instance_relative_config= True)
 # Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import highlight
