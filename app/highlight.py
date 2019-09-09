448403c0c389860fa2adcedacc888edceddd0e07
happiness <happinesskawinzi@gmail.com>
2019-09-09 09:22:16 +0300

    create a new dynamic route for our new_review function and pass in the news id

--- app/highlight.py
+++ app/highlight.py
@@ -2,6 +2,10 @@ from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_views
 from .request import get_views,get_view,search_view
+from .models import review
+from .forms import ReviewForm
+
+Review = review.Review
 
 @app.route('/')
 def index():
@@ -42,3 +46,18 @@ def search(view_name):
     searched_views = search_view(view_name_format)
     title = f'search results for {view_name}'
     return render_template('search.html',views = searched_views)
+
+@app.route('/view/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    view = get_view(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(view.author,title,description,url,urlToImage,publishedAt,content)
+        new_review.save_review()
+        return redirect(url_for('view',id = view.id ))
+
+    title = f'{view.title} review'
+    return render_template('new_review.html',title = title, review_form=form, view=view)
